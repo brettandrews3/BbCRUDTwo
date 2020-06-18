1660e77380d843f4609f4aad6c9157ce721c4848
Brett Andrews <brettandrews3@gmail.com>
2020-06-18 11:21:17 -0500

    GetAllDepts() conn set for MySql

--- BbCRUDTwo/Program.cs
+++ BbCRUDTwo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace BbCRUDTwo
 {
@@ -6,7 +9,43 @@ namespace BbCRUDTwo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var departments = GetAllDepartments();
+
+            foreach(var dept in departments)
+            {
+                Console.WriteLine(dept);
+            }
+        }
+
+        //Let's establish our secure connection to the database:
+        static IEnumerable GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            //Our conn here tells the app to use ConnectionString.txt to see how
+            //to access the database:
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            //Here, use the connection conn to submit the 1st SQL command: SELECT NAME...;
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT Name FROM Departments;";
+
+            using(conn) //start the using statement and utilize MySqlConnection conn
+            {
+                conn.Open(); //Connection opens
+                List<string> allDepartments = new List<string>();
+                //Create new List<strin> to hold the departments from Departments table
+
+                //Reads a forward-only stream of rows, one at a time
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while(reader.Read() == true) //read while there's more depts to show
+                {
+                    var currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+                //List is now done, connection is closed, and show allDepartments
+                return allDepartments;
+            }
         }
     }
 }
