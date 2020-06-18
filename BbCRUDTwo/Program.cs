ca7d81e39e6ed1a07ef82316d00b931f3a5e9ef2
Brett Andrews <brettandrews3@gmail.com>
2020-06-18 10:59:29 -0500

    gitignore set

---
+++ BbCRUDTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BbCRUDTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
